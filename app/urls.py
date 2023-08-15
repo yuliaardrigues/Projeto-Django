b504de24878a931535667de4a70dcb5d78175ac5
Yulia Rayane Rodrigues <yuliarodrigues@aluno.unifapce.edu.br>
2023-08-15 16:37:59 -0300

    removido contato e sobre

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from app.views import home, contato, sobre 
+from app.views import home
 
 urlpatterns = [
     path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
+  
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,6 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
-   return render(request,'home.html')
-
-def contato(request):
-   return HttpResponse('contato')
-
-
-def sobre(request):
-   return HttpResponse('sobre')
+   return render(request,'recipes/home.html')
