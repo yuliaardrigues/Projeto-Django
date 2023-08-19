2f14009eae82b18169305f137755437e6b8ef391
Yulia Rayane Rodrigues <yuliarodrigues@aluno.unifapce.edu.br>
2023-08-19 01:27:35 -0300

    mudanças de pastas

--- app/views.py
+++ app/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 
 def home(request):
-   return render(request,'recipes/home.html')
+   
+   return render(request, 'recipes/pages/home.html')
--- projeto/settings.py
+++ projeto/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            / 'templates'
+            '/', 'templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
